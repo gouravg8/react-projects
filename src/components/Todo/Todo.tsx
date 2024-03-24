b413c4207fe7f613abf9ac52ccb394f87a173c47
Gourav soni <gouravsoni8476@gmail.com>
2024-03-24 23:02:27 +0530

    minor changes

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -7,12 +7,14 @@ import TodoStats from "./TodoStats";
 const Todo = () => {
   return (
     <Provider store={store}>
-      <div className="w-3/4 p-3 mx-auto my-24 align-middle bg-transparent md:w-1/2">
-        <h1 className="text-4xl font-semibold text-center text-gray-500 my-2">
-          THINGS TO DO
-        </h1>
-        <Todoinput />
-        <TodoList />
+      <div className="w-3/4 mx-auto my-24 align-middle bg-transparent md:w-1/2 border bg-white rounded">
+        <div className="px-8">
+          <h1 className="text-4xl font-semibold text-center text-gray-500 my-6">
+            THINGS TO DO
+          </h1>
+          <Todoinput />
+          <TodoList />
+        </div>
         <TodoStats />
       </div>
     </Provider>
--- src/components/Todo/TodoStats.tsx
+++ src/components/Todo/TodoStats.tsx
@@ -1,5 +1,16 @@
+import { useSelector } from "react-redux";
+import { todoTypes } from "../../features/todo/todoSlice";
+
 function TodoStats() {
-  return <div>TodoStats</div>;
+  const todos = useSelector((state: { todos: todoTypes[] }) => state.todos);
+  const done = useSelector(
+    (state: { doneTodoLength: number }) => state.doneTodoLength
+  );
+  return (
+    <div className="flex justify-center px-6 text-gray-500 bg-green-100 py-2 text-sm border">
+      <span>{todos.length - done} items left</span>
+    </div>
+  );
 }
 
 export default TodoStats;

49cdd4b50864cdef8ea5a6689d1e2e39b5bd4ff7
Gourav soni <gouravsoni8476@gmail.com>
2024-03-26 21:30:27 +0530

    added gallary compo

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,2 +1,3 @@
 export { Todo } from "./Todo/Todo";
 export { Calculator } from "./Calculator/Calculator";
+export { Gallary } from "./Gallary/Gallary";
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Todo from "./Todo/Todo";
 import Calculator from "./Calculator/Calculator";
+import Gallary from "./Gallary/Gallary";
 
-export { Todo, Calculator };
+export { Todo, Calculator, Gallary };
